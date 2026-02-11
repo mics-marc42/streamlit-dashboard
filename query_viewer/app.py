29c1e9c35eae7d2708474a8350ea86038e28374f
mic <fyanmi007@gmail.com>
2026-02-11 23:01:57 +0530

    Filters for PT report

--- query_viewer/app.py
+++ query_viewer/app.py
@@ -5,7 +5,7 @@ from bigquery_utils import query_bigquery
 
 st.set_page_config(page_title="Query Viewer", layout="wide")
 
-st.title("OPA DAILY TRACKER")
+st.title("BigQuery Query Viewer")
 
 tab1, tab2 = st.tabs(["PT order tracker", "Agent efficiency tracker"])
 
